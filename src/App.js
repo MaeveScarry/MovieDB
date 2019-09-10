42c82f148bf479923d3bf3445e180affec5ce662
MaeveScarry <maevescarry@googlemail.com>
2019-09-10 17:15:43 +0100

    Revert "add jumbotron"
    This reverts commit 49e10513f1bf229021393fb9ef49be321010c4e0.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Button, Jumbotron } from 'react-bootstrap';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-<Jumbotron>
-  <h1>Hello, world!</h1>
-  <p>
-    This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.
-  </p>
-  <p>
-    <Button variant="primary">Learn more</Button>
-  </p>
-</Jumbotron>      
-      
-        
-   
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="Start">
+          Welcome to the MovieDB
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
