9201d9915b03d9deb537813deed8510c3af7d7eb
MaeveScarry <maevescarry@googlemail.com>
2019-09-13 16:48:14 +0100

    createdcontainerandcomponentfolders

---
+++ src/container/homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../../component/hero/Hero";
+import "./Homepage.css";
+
+function Homepage() {
+  return (
+    <div className="App">
+      <Hero />
+    </div>
+  );
+}
+
+export default Homepage;
