a4fb61d1f740ca0145e0b6f862b872d1e4f122fc
MaeveScarry <maevescarry@googlemail.com>
2019-10-04 16:25:18 +0100

    Fetching the film info

--- src/component/hero/Hero.js
+++ src/component/hero/Hero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 import InputGroup from "react-bootstrap/InputGroup";
@@ -7,26 +7,47 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "./Hero.css";
 
-function Hero() {
-  return (
-    <Jumbotron fluid>
-      <Container> 
-        <h1 className="hero-heading">Find YOUR film</h1>
-        <InputGroup className="mb-3">
-          <InputGroup.Prepend>
-            <InputGroup.Text id="basic-addon1">
-              <FontAwesomeIcon icon={faSearch} />
-            </InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl
-            placeholder="Film title"
-            aria-label="Film title"
-            aria-describedby="basic-addon1"
-          />
-        </InputGroup>
-      </Container>
-    </Jumbotron>
-  );
+const API = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIEDB_API}&query=`;
+const QUERY = 'Titanic';
+
+console.log(process.env.REACT_APP_MOVIEDB_API)
+
+class Hero extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch(API+QUERY)
+    .then(response => response.json())
+    .then(data => {console.log(data)})
+  }
+
+  render() {
+    return (
+      <Jumbotron fluid>
+        <Container> 
+          <h1 className="hero-heading">Find YOUR film</h1>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon1">
+                <FontAwesomeIcon icon={faSearch} />
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              placeholder="Film title"
+              aria-label="Film title"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+        </Container>
+      </Jumbotron>
+    );
+  }
 }
 
 export default Hero;
--- src/container/homepage/Homepage.js
+++ src/container/homepage/Homepage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from "../../component/hero/Hero";
-import Moviecards from "../../component/moviecards/Moviecards"
+import Moviecards from "../../component/moviecards/Moviecards";
 import "./Homepage.css";
 
 function Homepage() {
